31fa7abdaf899cf4cf48f84255cc52ecbe1cb272
avu12 <algebra3eloadas@gmail.com>
2020-06-29 22:22:03 +0200

    more JS exercises

--- Javascript/first.js
+++ Javascript/first.js
@@ -1,107 +1,28 @@
-function sayHello(){
-    console.log("Hi");
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+button.addEventListener("click",function(){
+    //console.log("Mouse clicked");
+})
+
+
+function addElement(){
+    if(input.value.length > 0 ){
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(input.value));
+        var lista = document.getElementById("list").appendChild(li);
+        input.value="";
+    }  
 }
-//other way:
-var sayBye = function(){
-    console.log("Bye");
-}
 
-4 + 3;
 
-if (4+3 ===7){
-//alert("GG EZ");
-console.log("Example log")
-}
-sayHello();
-sayBye();
+button.addEventListener("click",function(){
+    addElement();
+})
 
-var list = ["a","b","c"]
-var list2 = ["a",list, undefined,true]
-console.log(list[2])
-console.log(list2[1])
-
-/*var user ={
-    name : "John",
-    age : 56,
-    shout:function(){
-        console.log("AAAAA");
-    }
-};
-
-console.log(user.name);
-user.shout();*/
-nullO = null;
-emptyO = {};
-emptyO.name = "Not so empty anymore";
-console.log(emptyO.name)
-
-//EXERICE
-
-var db =[
+input.addEventListener("keypress",function(event){
+    if (event.keyCode === 13)
     {
-        username : "Peter",
-        password : "SupaSecret123",
-    }
-]
-
-var newsFeed = [
-    {
-        username : "Bobby",
-        timeline : "Bulls*it",
-    },
-
-]
-var userNamePrompt = prompt("Username:");
-var pwd = prompt("What is your password?");
-signIn(userNamePrompt,pwd);
-
-
-for (var i =0; i<newsFeed.length;i++){
-    console.log(newsFeed[i]);
-}
-
-var counter =10;
-
-while(counter >0){
-console.log(counter);
-counter--;
-} 
-/*
-newsFeed.forEach(function(newsFeed,i){
-    console.log(newsFeed.username);
-});*/
-
-var a = ["a", "b", "c"];
-a.forEach(function(entry) {
-    console.log(entry);
-});
-
-
-
-var a  = document.querySelector("h1");
-console.log(a);
-
-
-
-
-
-
-
-
-
- function signIn(user,pass){
-    if( user === db[0].username && pass === db[0].password)
-    {
-        alert("Logged in!");
-        console.log(newsFeed);
-        return;
-    }
-    else{
-        alert("Invalid username/password");
-        return;
+        addElement();
     }
     
-
- }
-
-
+})
